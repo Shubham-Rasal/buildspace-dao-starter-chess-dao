f21509c441c5ad517a5d037be9aa9109960cf74f
Shubham-Rasal <bluequbits@gmail.com>
2022-07-03 12:17:28 +0530

    First commit after fork

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import { useAddress,useMetamask } from "@thirdweb-dev/react";
+
+
 const App = () => {
+
+  const address = useAddress();
+  const connectWithWallet = useMetamask();
+  console.log("Address: ", address);
+
+  if(!address)
+  {
+    return (
+      <div>
+        <button onClick={connectWithWallet}>Connect with Wallet</button>
+      </div>
+    );
+  }
   return (
     <div className="landing">
-      <h1>Welcome to My DAO</h1>
+      <h1>Welcome to My DAO.
+        😎
+      </h1>
     </div>
   );
 };
