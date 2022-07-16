04014aeb2b48d1651531aaf85eec3be296e75c1e
Shubham-Rasal <bluequbits@gmail.com>
2022-07-16 22:12:23 +0530

    Revoking admin rights

---
+++ scripts/11-revoke-roles.js
@@ -0,0 +1,26 @@
+
+import sdk from "./1-initialize-sdk.js"
+
+const token = sdk.getToken("0xde4f1fd54d980616ddBf28754cB2165b93Da47d3");
+
+(async()=>{
+    try {
+
+        const allRoles  = await token.roles.getAll();
+
+        console.log("All roles ", allRoles);
+
+        await token.roles.setAll({admin:[],minter:[]});
+
+        console.log("All roles ", await token.roles.getAll());
+
+        
+        console.log("✅ Successfully revoked my superpowers from the ERC-20 contract");
+
+    }
+    catch (e)
+    {
+        console.error("Could not revoke roles.", e);
+        
+    }
+})();
